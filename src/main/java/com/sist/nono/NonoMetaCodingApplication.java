b7f082c05baf103cd131e4b7126756b1c5449e50
wiufixo <104910435+wiufixo@users.noreply.github.com>
2022-06-29 00:35:34 +0900

    board and user with jpa

---
+++ src/main/java/com/sist/nono/NonoMetaCodingApplication.java
@@ -0,0 +1,13 @@
+package com.sist.nono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonoMetaCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonoMetaCodingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sist/nono/test/TestController.java
@@ -0,0 +1,14 @@
+package com.sist.nono.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello spring boot!!</h1>";
+	}
+}
